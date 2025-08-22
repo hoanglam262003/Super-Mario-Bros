34a9a61c735f227ef3636db1bc30d0fe3095daea
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-08-22 18:06:50 +0700

    player movement

--- Assets/Scripts/Extension.cs
+++ Assets/Scripts/Extension.cs
@@ -13,7 +13,13 @@ public static class Extension
         float radius = 0.25f;
         float distance = 0.375f;
 
-        RaycastHit2D hit = Physics2D.CircleCast(rb.position, radius, direction, distance, layerMask);
+        RaycastHit2D hit = Physics2D.CircleCast(rb.position, radius, direction.normalized, distance, layerMask);
         return hit.collider != null && hit.rigidbody != rb;
+    }
+
+    public static bool DotTest(this Transform transform, Transform other, Vector2 testDirection)
+    {
+        Vector2 direction = other.position - transform.position;
+        return Vector2.Dot(direction.normalized, testDirection) > 0.25f;
     }
 }
