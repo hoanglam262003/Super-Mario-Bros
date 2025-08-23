f8f8bac28114ffddc8e413babf8abcad7952ac3f
Vu Le Lam Hoang <hoanglam262003@gmail.com>
2025-08-23 16:56:39 +0700

    enemy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int world { get; private set; }
+    public int stage { get; private set; }
+    public int lives { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        loadLevel(1, 1);
+    }
+
+    public void loadLevel(int world, int stage)
+    {
+        this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}");
+    }
+
+    public void NextLevel()
+    {
+        // need implement for multiple worlds
+        loadLevel(world, stage + 1);
+    }
+
+    public void ResetLevel(float delay)
+    {
+        Invoke(nameof(ResetLevel), delay);
+    }
+
+    public void ResetLevel()
+    {
+        lives--;
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            loadLevel(world, stage);
+        }
+    }
+
+    private void GameOver()
+    {
+        NewGame();
+    }
+}
